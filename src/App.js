519660d00a6949b8faadec8d866abe9fa0de2417
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-09 18:35:42 +0800

    Added color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Palette from "./components/Palette/Palette";
-import seedColors from "./seedColors";
+import seedColors from "./helpers/seedColors";
+
+import { generatePalette } from "./helpers/colorHelpers";
 
 export default function App(){
+  console.log(generatePalette(seedColors[4]));
   return(
     <Palette {...seedColors[0]}/>
   );
