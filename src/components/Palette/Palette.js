9a6dd15860b07189d03e3fd4bc244d73b9cc8b96
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-09 20:36:52 +0800

    Styled color slider

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
-import './Palette.css';
+
 //
 import ColorBox from "../ColorBox/ColorBox";
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './Palette.css';
 
 export default function Palette({palette}){
   const [level, setLevel] = useState(500);
@@ -25,13 +26,15 @@ export default function Palette({palette}){
 
   return(
     <div className="palette">
-      <Slider 
-        defaultValue={level} 
-        min={100} 
-        max={900}
-        step={100}
-        onAfterChange={changeLevel}
-      />
+      <div className='slider'>
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={changeLevel}
+        />
+      </div>
       {/* navbar goes here */}
       <div className='palette-colors'>
         {colorBoxes}
