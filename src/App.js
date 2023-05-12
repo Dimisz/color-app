06f333eb1de371fd0bd9e9b2af864c4d8cfaad9c
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-12 11:10:55 +0800

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Palette from "./components/Palette/Palette";
 import seedColors from "./helpers/seedColors";
-
 import { generatePalette } from "./helpers/colorHelpers";
+import { Route, Routes } from 'react-router-dom';
+
+const InividualPalette = () => {
+  return <h1>Individual palette</h1>
+}
 
 export default function App(){
   console.log(generatePalette(seedColors[4]));
   return(
-    <Palette palette={generatePalette(seedColors[4])}/>
+    <Routes>
+      <Route path='/palette' element={
+        <Palette 
+          palette={generatePalette(seedColors[4])}
+        />} 
+      />
+      <Route path='/palette/:id' element={
+        <InividualPalette/>} 
+      />
+    </Routes>
+    // 
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
@@ -7,5 +8,7 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <App/>
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 );
